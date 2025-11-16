7357f433f0a4a7c6e015505d0adf6f015c02d53a
lucienjll <llin@olin.edu>
2025-11-16 16:35:00 -0500

    simulate box

--- simulate_box.m
+++ simulate_box.m
@@ -1,5 +1,5 @@
 function simulate_box()
-    LW = 10; LH = 1; LG = 3;
+    LW = 10; LH = 10; LG = 3;
     m = 1; Ic = (1/12)*(LH^2+LW^2);
     g = 1; k = 20; k_list = [.5*k,.5*k,2*k,5*k];
     l0 = 1.5*LG;
@@ -12,8 +12,16 @@ function simulate_box()
     Pbl2_world = Pbl_box + [LG;-LG];
     Pbr1_world = Pbr_box + [0;-l0];
     Pbr2_world = Pbr_box + [l0;0];
-    P_world = [Pbl1_world,Pbl2_world,Pbr1_world,Pbr2_world];
-    P_box = [Pbl_box,Pbl_box,Pbr_box,Pbr_box];
+    
+    Ptl1_world = Ptl_box + [LG;LG];
+    Ptl2_world = Ptl_box + [-LG;LG];
+    Ptr1_world = Ptr_box + [0;-l0];
+    Ptr2_world = Ptr_box + [10;0];
+
+    P_world = [Pbl1_world,Pbl2_world,Pbr1_world,Pbr2_world,...
+        Ptl1_world,Ptl2_world,Ptr1_world,Ptr2_world];
+    P_box = [Pbl_box,Ptl_box,Pbr_box,Ptr_box];
+
     %define system parameters
     box_params = struct();
     box_params.m = m;
@@ -53,6 +61,66 @@ function simulate_box()
     V0 = [x0;y0;theta0;vx0;vy0;vtheta0];
     tspan = [0 10];    %your code here
     %run the integration
-    [t_list,X_list,h_avg, num_evals, fail_rate, h_rec] = explicit_RK_variable_step_integration(my_rate_func,tspan,V0,h_ref,Fehlberg, p, error_desired);
-    plot(X_list(1,:), X_list(2,:))
+    [t_list,X_list,~, ~, ~, ~] = explicit_RK_variable_step_integration(my_rate_func,tspan,V0,h_ref,Fehlberg, p, error_desired);
+    
+    %simulating box
+
+    box_plot_struct = initialize_box_plot();
+    
+    num_zigs = 5;
+    w = .1;
+    hold on;
+    spring_plot_struct1 = initialize_spring_plot(num_zigs,w);
+    spring_plot_struct2 = initialize_spring_plot(num_zigs,w);
+    spring_plot_struct3 = initialize_spring_plot(num_zigs,w);
+    spring_plot_struct4 = initialize_spring_plot(num_zigs,w);
+
+    spring_plot_struct5 = initialize_spring_plot(num_zigs,w);
+    spring_plot_struct6 = initialize_spring_plot(num_zigs,w);
+    spring_plot_struct7 = initialize_spring_plot(num_zigs,w);
+    spring_plot_struct8 = initialize_spring_plot(num_zigs,w);
+
+    axis equal; axis square;
+    axis([-3,3,-3,3]);
+
+    % P_world = [Pbl1_world,Pbl2_world,Pbr1_world,Pbr2_world,...
+    %     Ptl1_world,Ptl2_world,Ptr1_world,Ptr2_world];
+    % P_box = [Pbl_box,Ptl_box,Pbr_box,Ptr_box];
+    for i = length(t_list)
+        P1 = Pbl1_world;
+        P2 = Pbl_box;
+        update_spring_plot(spring_plot_struct1,P1,P2)
+
+        P1 = Pbl2_world;
+        P2 = Pbl_box;
+        update_spring_plot(spring_plot_struct2,P1,P2)
+
+        P1 = Pbr1_world;
+        P2 = Pbr_box;
+        update_spring_plot(spring_plot_struct3,P1,P2)
+
+        P1 = Pbr2_world;
+        P2 = Pbr_box;
+        update_spring_plot(spring_plot_struct4,P1,P2)
+
+        P1 = Ptl1_world;
+        P2 = Ptl_box;
+        update_spring_plot(spring_plot_struct5,P1,P2)
+
+        P1 = Ptl2_world;
+        P2 = Ptl_box;
+        update_spring_plot(spring_plot_struct6,P1,P2)
+
+        P1 = Ptr1_world;
+        P2 = Ptr_box;
+        update_spring_plot(spring_plot_struct7,P1,P2)
+
+        P1 = Ptr2_world;
+        P2 = Ptr_box;
+        update_spring_plot(spring_plot_struct8,P1,P2)
+
+
+        %update_box_plot(box_plot_struct,box_params)
+        drawnow;
+    end
 end
--- update_box_plot.m
+++ update_box_plot.m
@@ -1,5 +1,5 @@
 function update_box_plot(box_plot_struct,box_params)
-    plot_pts = box_params.P_world;
+    plot_pts = box_params.P_box;
     set(box_plot_struct.line_plot,'xdata',plot_pts(1,:),'ydata',plot_pts(2,:));
     set(box_plot_struct.point_plot,'xdata',plot_pts(1,:),'ydata',plot_pts(2,:));
 end
